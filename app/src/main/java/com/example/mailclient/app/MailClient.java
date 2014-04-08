808d48d2bba93dab6a81c06324dfdda11781eb48
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2014-04-08 17:27:15 +0200

    Drawer layout

--- app/src/main/java/com/example/mailclient/app/MailClient.java
+++ app/src/main/java/com/example/mailclient/app/MailClient.java
@@ -3,8 +3,10 @@ package com.example.mailclient.app;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.Menu;
@@ -12,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -45,9 +48,10 @@ public class MailClient extends Activity {
     /*
      *  Drawer menu variables
      */
-    private String[] mPlanetTitles;
+    private String[] mDrawerLinks;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     public MailClient() {
     }
@@ -57,9 +61,29 @@ public class MailClient extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mPlanetTitles = getResources().getStringArray(R.array.sidebar);
+        /*
+         *  Implement drawer layout and adapters
+         */
+        mDrawerLinks = getResources().getStringArray(R.array.sidebar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mDrawerLinks));
+        // Set the list's click listener
+//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout, R.drawable.ic_drawer , R.string.drawer_open, R.string.drawer_close){
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+            public void onDrawerOpened(View view) {
+                super.onDrawerOpened(view);
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
 
         /*
          *  Needed for saving methods
@@ -130,6 +154,17 @@ public class MailClient extends Activity {
         mPocketBar.progressiveStop();
     }
 
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
     //inflate the menu with custom actions
     @Override
@@ -144,6 +179,9 @@ public class MailClient extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
