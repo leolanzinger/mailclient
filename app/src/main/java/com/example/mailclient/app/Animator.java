301e74f977cda9b06795098c19ecdd6f59dcf4a5
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2014-04-30 17:03:04 +0200

    fixed swipe animation

--- app/src/main/java/com/example/mailclient/app/Animator.java
+++ app/src/main/java/com/example/mailclient/app/Animator.java
@@ -1,5 +1,7 @@
 package com.example.mailclient.app;
 
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.Point;
 import android.view.Display;
@@ -18,29 +20,10 @@ public class Animator {
 
     public void resetView(final View v) {
         // reset della view a posizione iniziale
-        TranslateAnimation anim = new TranslateAnimation(0, -v.getX(), 0, 0);
-        anim.setFillAfter(false);
+        ObjectAnimator anim = ObjectAnimator.ofFloat(v, "translationX", v.getX(), 0);
+
         anim.setDuration(500);
-
-        anim.setAnimationListener(new Animation.AnimationListener() {
-
-            @Override
-            public void onAnimationStart(Animation animation) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                v.setX(0);
-            }
-        });
-
-        v.startAnimation(anim);
+        anim.start();
     }
 
     /*
@@ -53,29 +36,33 @@ public class Animator {
         display.getSize(size);
         int screen_width = size.x;
 
-        // reset della view a posizione iniziale
-        TranslateAnimation anim = new TranslateAnimation(0, screen_width, 0, 0);
-        anim.setFillAfter(false);
-        anim.setDuration(500);
-        anim.setAnimationListener(new Animation.AnimationListener() {
+        // elimina la view
+        ObjectAnimator anim = ObjectAnimator.ofFloat(v, "translationX", v.getX(), screen_width);
+        anim.addListener(new AnimatorListenerAdapter() {
 
             @Override
-            public void onAnimationStart(Animation animation) {
-                // TODO Auto-generated method stub
+            public void onAnimationStart(android.animation.Animator animator) {
+
             }
 
             @Override
-            public void onAnimationRepeat(Animation animation) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
+            public void onAnimationEnd(android.animation.Animator animator) {
                 Todo.todo_list.remove(pos);
                 Todo.adapter.notifyDataSetChanged();
                 v.setX(0);
             }
+
+            @Override
+            public void onAnimationCancel(android.animation.Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(android.animation.Animator animator) {
+
+            }
         });
-        v.startAnimation(anim);
+        anim.setDuration(500);
+        anim.start();
     }
 }
