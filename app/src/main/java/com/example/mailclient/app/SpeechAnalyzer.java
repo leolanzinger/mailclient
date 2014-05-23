c9937832bcf41b80f8b5d8a7aee520e043fd9958
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2014-05-23 13:55:44 +0200

    title giusto

--- app/src/main/java/com/example/mailclient/app/SpeechAnalyzer.java
+++ app/src/main/java/com/example/mailclient/app/SpeechAnalyzer.java
@@ -18,20 +18,49 @@ public class SpeechAnalyzer {
 
     public void analyze(ArrayList<String> result, Activity activity, Email email) {
         ArrayList<String> myList = new ArrayList<String>(Arrays.asList(result.get(0).split("\\s+")));
+        Log.i("voice", myList.toString());
         if (myList.get(0).equals("rispondi")) {
-            if (myList.get(1).equals("tutti")){
+            myList.remove(0);
+            if (myList.get(0).equals("a") && myList.get(1).equals("tutti") && !myList.get(2).equals("che")){
                 myList.remove(0);
-                myList.remove(1);
-                String reply = myList.toString();
+                myList.remove(0);
+                String reply = "";
+                for (String s : myList)
+                {
+                    reply += s + " ";
+                }
+                reply += "\n";
                 ((ReadMail) activity).replyAll(reply);
+                Log.i("voice", "rispondi a tutti");
+            }
+            else if (myList.get(0).equals("a") && myList.get(1).equals("tutti") && myList.get(2).equals("che")){
+                myList.remove(0);
+                myList.remove(0);
+                myList.remove(0);
+                String reply = "";
+                for (String s : myList)
+                {
+                    reply += s + " ";
+                }
+                reply += "\n";
+                ((ReadMail) activity).replyAll(reply);
+                Log.i("voice", "rispondi a tutti che");
             }
             else {
-                myList.remove(0);
-                String reply = myList.toString();
+                if (myList.get(0).equals("che")) {
+                    myList.remove(0);
+                }
+                String reply = "";
+                for (String s : myList)
+                {
+                    reply += s + " ";
+                }
+                reply += "\n";
                 ((ReadMail) activity).replyMail(reply);
+                Log.i("voice", "rispondi e basta");
             }
         }
-        else if (myList.get(0).equals("ricordamelo")) {
+        else if (myList.get(0).equals("ricordamelo") || myList.get(0).equals("ricorda")) {
             if (myList.get(1).equals("oggi")){
                 GregorianCalendar today_date = new GregorianCalendar();
                 // Add email to todolist
@@ -59,6 +88,12 @@ public class SpeechAnalyzer {
             }
             activity.findViewById(R.id.readMail_pin).setBackgroundColor(R.color.yellow);
             activity.findViewById(R.id.readMail_pin).setBackgroundResource(R.drawable.pinned);
+        }
+        else {
+            //set toast
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(activity, "Sorry, I can't understand your command", duration);
+            toast.show();
         }
     }
 }
